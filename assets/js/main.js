c79345e02aa8f665efd407dee57d66013eb5bf0c
Raihan Fatih Dzaky <raihandzaky98@gmail.com>
2025-02-26 21:38:24 +0700

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
+// Change Background Header
+function scrollHeader() {
+    const header = document.getElementById('header');
+    // when the scroll is greater than 80 viewport height, add the scroll-header class to header tag
+    if (this.scrollY >= 80) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header');
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+// ===== Show Scroll Up =====
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // when the scroll is greater than 350 viewport height, add the show-scroll class to scroll-top class
+    if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll', scrollUp)
+
 // ===== About Tabs =====
 const tabs = document.querySelectorAll('[data-target]');
  tabContents = document.querySelectorAll('[data-content]');
